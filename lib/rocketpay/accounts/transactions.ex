8ba34d065acde19174edd44e4e16203b1ead0049
avatar9966 <av4t@hotmail.com>
2021-02-25 20:48:59 -0300

    refactor: change run transaction to return a struct

--- lib/rocketpay/accounts/transactions.ex
+++ lib/rocketpay/accounts/transactions.ex
@@ -2,6 +2,7 @@ defmodule Rocketpay.Accounts.Transaction do
   alias Ecto.Multi
 
   alias Rocketpay.Accounts.Operation
+  alias Rocketpay.Accounts.Transactions.Reponse, as: TransactionResponse
   alias Rocketpay.Repo
 
   def call(%{"from" => from_id, "to" => to_id, "value" => value}) do
@@ -19,7 +20,9 @@ defmodule Rocketpay.Accounts.Transaction do
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
       {:error, _operation, reason, _changes} -> {:error, reason}
-      {:ok, %{deposit: to_account, withdraw: from_account}} -> {:ok, %{to_account: to_account, from_account: from_account}}
+
+      {:ok, %{deposit: to_account, withdraw: from_account}} ->
+        {:ok, TransactionResponse.build(from_account, to_account)}
     end
   end
 end
